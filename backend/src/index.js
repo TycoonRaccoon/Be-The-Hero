e176603cf2bd15cee00075c171f94c1872a9bc14
Gabriel Nori <gabriel.nori@arislab.com.br>
2020-03-24 21:59:43 +0000

    creating backend

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const routes = require('./routes')
+const cors = require('cors')
+
+
+const app = express()
+app.use(express.json())
+
+app.use(cors())
+app.use(routes)
+
+
+const port = 3333
+app.listen(`Server running at port ${port}`, (err) => { if(err) console.log(err) })
